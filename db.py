607b6ed15bbef75b86e17d292786ca503241dd05
Hwang Youseong <hysskm@gmail.com>
2025-07-24 15:18:36 +0900

    Create_account

--- db.py
+++ db.py
@@ -1,13 +1,14 @@
 import pymysql
 import os
-from pymysql import Error
+from pymysql import Error, IntegrityError
 from dotenv import load_dotenv
+
+
 
 load_dotenv()
 
 class Database:
-    def __init__(self):
-        
+    def __init__(self):  
         self.connection = None
         try:
             self.connection = pymysql.connect(
@@ -24,3 +25,26 @@ class Database:
             print(f"MariaDB 연결 중 오류 발생: {e}")
     
     # TODO: DB 컨넥션 및 Model 관련 작업 필요
+    def create_account(self, email, password) -> bool:
+        if self.connection is None: # DB에 연결이 없을 때,
+            print("데이터 베이스 연결이 되지 않았습니다.")
+            return False
+        with self.connection.cursor as c:
+            try: # 중복 이메일이 들어왔을 때,
+                query = """INSERT INTO USER (email, password)
+                        VALUES (%s, %s);"""
+                c.execute(query, (email, password))
+            except IntegrityError as ie: # 중복 이메일을 다시 처리를 해줘야하는 문
+                print(f"중복된 이메일이 들어왔습니다.", {ie})
+            self.connection.commit()
+            print("DB에 데이터가 성공적으로 저장되었습니다.")
+            return True
+    
+    # Read Account List
+    def read_user_list(self):
+        if self.connection is None:
+            print("데이터 베이스 연결이 되지 않았습니다.")
+            query = """
+                SELECT (email, password) FROM USER
+                """
+
