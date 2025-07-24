34c7056e1f6d55c3b16928a76816d541bd3edbb7
Duhoon <eclip6@naver.com>
2025-07-24 15:09:05 +0900

    feat: 데이터베이스 컨넥션 오픈 코드 추가

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-# TODO: Router 및 Controller 구현 필요 
+from flask import Flask, render_template, request, redirect, url_for
+from db import Database
+import atexit
+
+app = Flask(__name__)
+db = Database()
+
+atexit.register(db.close)
+
+# TODO: Router 및 Controller 구현 필요
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
